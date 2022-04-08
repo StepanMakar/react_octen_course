752d27991919183db8dae93d737f758443c8d56f
StepanMakar <stepan.lisnov1998@gmail.com>
2022-04-08 16:12:31 +0300

    process

---
+++ src/components/Users/Users.js
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+
+
+export const Users = () => {
+    const [users, setUsers] = useState(null)
+    useEffect(() => {
+
+
+    }, [])
+    return (
+        <div>
+
+        </div>
+    );
+}
