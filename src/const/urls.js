4e6cda4391e5e5c6a8ef9172d9f24d2bb341938b
StepanMakar <stepan.lisnov1998@gmail.com>
2022-04-21 20:37:43 +0300

    process

---
+++ src/const/urls.js
@@ -0,0 +1,9 @@
+const  base_URL = process.env.REACT_APP_API_URL
+
+const urls = {
+    users: '/users',
+    comments: '/comments',
+    posts: '/posts'
+}
+export {urls}
+export default base_URL
---
+++ src/servise/axios.service.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+import base_URL from "../const/urls";
+
+const axios_service = axios.create({base_URL})
